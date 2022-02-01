639c4db2decf1b667218aaf97602e26ccca6b8c4
Ihustat <exm@gmail.com>
2022-02-01 15:49:21 +0300

    add modal

--- js/script.js
+++ js/script.js
@@ -92,4 +92,40 @@ function getTimeRemaining(endtime) {
    }
 
    setClock('.timer', deadline)
+
+   //modal
+
+   const modalTrigger = document.querySelectorAll('[data-modal]'),
+         modal = document.querySelector('.modal'),
+         modalCloseBtn = document.querySelector('[data-close]')  
+
+         modalTrigger.forEach(btn => {
+            btn.addEventListener('click', () => {
+                modal.classList.add('show')
+                modal.classList.remove('hide')
+                document.body.style.overflow = 'hidden'
+            })
+         })
+
+         function closeModal() {
+            modal.classList.add('hide')
+            modal.classList.remove('show')
+            document.body.style.overflow = ''
+         }
+
+  modalCloseBtn.addEventListener('click', closeModal)
+
+   modal.addEventListener('click', (event) => {
+       if (event.target === modal) {
+       closeModal()
+       }
+   })
+
+   document.addEventListener('keydown', (e) => {
+
+    if (e.code === 'Escape' && modal.classList.contains('show')) {
+        closeModal()
+    }
+   })
+
 })
