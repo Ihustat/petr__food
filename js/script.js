5a4a92e54f41f53a5cc6841dd96e7584ff4748a7
Ihustat <exm@gmail.com>
2022-02-02 11:20:00 +0300

    upgrade modal

--- js/script.js
+++ js/script.js
@@ -99,13 +99,18 @@ function getTimeRemaining(endtime) {
          modal = document.querySelector('.modal'),
          modalCloseBtn = document.querySelector('[data-close]')  
 
+         function openModal() {
+            modal.classList.add('show')
+            modal.classList.remove('hide')
+            document.body.style.overflow = 'hidden'
+            clearInterval(modalTimerId)
+         }
+
          modalTrigger.forEach(btn => {
-            btn.addEventListener('click', () => {
-                modal.classList.add('show')
-                modal.classList.remove('hide')
-                document.body.style.overflow = 'hidden'
-            })
+            btn.addEventListener('click', openModal)
          })
+
+        
 
          function closeModal() {
             modal.classList.add('hide')
@@ -127,5 +132,16 @@ function getTimeRemaining(endtime) {
         closeModal()
     }
    })
+
+   const modalTimerId = setTimeout(openModal, 3000)
+
+   function showModalByScroll() {
+    if (window.pageYOffset + document.documentElement.clientHeight >= document.documentElement.scrollHeight) {
+        openModal()
+        window.removeEventListener('scroll', showModalByScroll)
+    }
+   }
+
+   window.addEventListener('scroll', showModalByScroll)
 
 })
