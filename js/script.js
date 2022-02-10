0522debdc3cbbdcbc086d94aec27d645c0d8f117
Ihustat <exm@gmail.com>
2022-02-10 16:27:01 +0300

    live render cards

--- js/script.js
+++ js/script.js
@@ -187,16 +187,46 @@ function getTimeRemaining(endtime) {
        }
    }
 
-   new MenuCard(
-    "img/tabs/vegy.jpg", 
-    "vegy",
-    'Меню "Фитнес"', 
-    'Меню "Фитнес" - это новый подход к приготовлению блюд: больше свежих овощей и фруктов. Продукт активных и здоровых людей. Это абсолютно новый продукт с оптимальной ценой и высоким качеством!',
-    9,
-    '.menu .container',
-    'menu__item'
-   ).render()
+   const getResource = async (url) => {
+    const res = await fetch(url);
 
+    if (!res.ok) {
+       throw new Error(`Could not fetch ${url}, status: ${res.status}`)
+    }
+
+    return await res.json()
+}
+
+getResource('http://localhost:3000/menu')
+    .then(data => {
+        data.forEach(({img, altimg, title, descr, price}) => {
+            new MenuCard(img, altimg, title, descr, price, '.menu .container').render()
+        })
+    })
+
+
+// getResource('http://localhost:3000/menu')
+//     .then(data => createCard(data))
+
+//     function createCard(data) {
+//         data.forEach(({img, altimg, title, descr, price}) => {
+//             const element = document.createElement('div')
+//             element.classList.add('menu__item')
+//             price *= 27
+//             element.innerHTML = `
+//             <img src=${img} alt=${altimg}>
+//         <h3 class="menu__item-subtitle">${title}</h3>
+//         <div class="menu__item-descr">${descr}</div>
+//         <div class="menu__item-divider"></div>
+//         <div class="menu__item-price">
+//             <div class="menu__item-cost">Цена:</div>
+//             <div class="menu__item-total"><span>${price}</span> грн/день</div>
+//         </div>
+//         `
+
+//         document.querySelector('.menu .container').append(element)
+//         })
+//     }
    //forms
 
    const forms = document.querySelectorAll('form')
@@ -208,10 +238,22 @@ function getTimeRemaining(endtime) {
    }
 
    forms.forEach(item => {
-       postData(item)
+    bindPostData(item)
    })
 
-   function postData(form) {
+   const postData = async (url, data) => {
+       const res = await fetch(url, {
+        method: "POST",
+        headers: {
+           'Content-type': 'application/json'
+        },
+        body: data
+       });
+
+       return await res.json()
+   }
+
+   function bindPostData(form) {
        form.addEventListener('submit', (e) => {
         e.preventDefault()
 
@@ -227,22 +269,11 @@ function getTimeRemaining(endtime) {
         const formData = new FormData(form)
       
 
-        const object = {}
-        formData.forEach(function(value, key) {
-            object[key] = value
-    
-        })
+        const json = JSON.stringify(Object.fromEntries(formData.entries()))
+     
 
         
-
-        fetch('server.php', {
-            method: "POST",
-            headers: {
-               'Content-type': 'application/json'
-            },
-            body: JSON.stringify(object)
-        })
-        .then(data => data.text())
+        postData('http://localhost:3000/requests', json)
         .then(data => {
             console.log(data);
             showThanksModal(message.succes)
@@ -280,6 +311,7 @@ function getTimeRemaining(endtime) {
             closeModal()
         }, 4000)
     }
+
 
 
 })
