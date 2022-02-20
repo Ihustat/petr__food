f58d566bbdc392e52111a3cd67e1e1c78a9f017a
Ihustat <exm@gmail.com>
2022-02-20 17:06:24 +0300

    add simple slider

--- js/script.js
+++ js/script.js
@@ -1,3 +1,5 @@
+
+
 window.addEventListener('DOMContentLoaded', ()=> {
 //tabs
 const tabs = document.querySelectorAll('.tabheader__item'),
@@ -227,6 +229,8 @@ getResource('http://localhost:3000/menu')
 //         document.querySelector('.menu .container').append(element)
 //         })
 //     }
+
+
    //forms
 
    const forms = document.querySelectorAll('form')
@@ -312,9 +316,61 @@ getResource('http://localhost:3000/menu')
         }, 4000)
     }
 
+    //slider
 
+    const slides = document.querySelectorAll('.offer__slide'),
+          prev = document.querySelector('.offer__slider-prev'),
+          next = document.querySelector('.offer__slider-next'),
+          current = document.querySelector('#current'),
+          total = document.querySelector('#total')
+    let slideIndex = 1
+       
+
+        showSlides(slideIndex)
+
+        if (slides.length < 10) {
+            total.textContent = `0${slides.length}`
+        } else {
+            total.textContent = slides.length
+        }
+      
+    function showSlides(n) {
+     if (n > slides.length) {
+         slideIndex = 1
+     }
+
+     if (n < 1) {
+         slideIndex = slides.length
+     }
+
+        slides.forEach(item => {
+            item.classList.add('hide')
+        })
+
+        slides[slideIndex - 1].classList.remove('hide')
+
+        if (slides.length < 10) {
+            current.textContent = `0${slideIndex}`
+        } else {
+            current.textContent = slideIndex
+        }
+    }
+
+    function plusSlides(n) {
+        showSlides(slideIndex += n)
+    }
+
+    prev.addEventListener('click', () => {
+        plusSlides(-1)
+    })
+    next.addEventListener('click', () => {
+        plusSlides(1)
+    })
+   
 
 })
+
+    
 
 
 
