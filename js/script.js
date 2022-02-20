d920d6e2439635eacc042d34edc7877a3bb784f2
Ihustat <exm@gmail.com>
2022-02-20 17:50:04 +0300

    upgrade skider

--- js/script.js
+++ js/script.js
@@ -322,50 +322,120 @@ getResource('http://localhost:3000/menu')
           prev = document.querySelector('.offer__slider-prev'),
           next = document.querySelector('.offer__slider-next'),
           current = document.querySelector('#current'),
-          total = document.querySelector('#total')
+          total = document.querySelector('#total'),
+          slidesWrapper = document.querySelector('.offer__slider-wrapper'),
+          slidesField = document.querySelector('.offer__slider-inner'),
+          width = window.getComputedStyle(slidesWrapper).width
+
     let slideIndex = 1
-       
+    let offset = 0
 
-        showSlides(slideIndex)
+    if (slides.length < 10) {
+                total.textContent = `0${slides.length}`
+                current.textContent = `0${slideIndex}`
+            } else {
+                total.textContent = slides.length
+                current.textContent = slideIndex
+            }
 
-        if (slides.length < 10) {
-            total.textContent = `0${slides.length}`
+    slidesField.style.width = 100 * slides.length + '%'
+    slidesField.style.display = 'flex'
+    slidesField.style.transition = '0.5s all'
+
+    slidesWrapper.style.overflow = 'hidden'
+
+    slides.forEach(slide => {
+        slide.style.width = width
+
+   
+    })
+
+    next.addEventListener('click', () => {
+        if (offset == +width.slice(0, width.length - 2) * (slides.length - 1)) {
+            offset = 0
         } else {
-            total.textContent = slides.length
+            offset += +width.slice(0, width.length - 2)
         }
-      
-    function showSlides(n) {
-     if (n > slides.length) {
-         slideIndex = 1
-     }
 
-     if (n < 1) {
-         slideIndex = slides.length
-     }
+        slidesField.style.transform = `translateX(-${offset}px)`
 
-        slides.forEach(item => {
-            item.classList.add('hide')
-        })
-
-        slides[slideIndex - 1].classList.remove('hide')
+        if (slideIndex == slides.length) {
+            slideIndex = 1
+        } else {
+            slideIndex++
+        }
 
         if (slides.length < 10) {
             current.textContent = `0${slideIndex}`
         } else {
             current.textContent = slideIndex
         }
-    }
-
-    function plusSlides(n) {
-        showSlides(slideIndex += n)
-    }
+    })
 
     prev.addEventListener('click', () => {
-        plusSlides(-1)
+        if (offset == 0) {
+            offset = +width.slice(0, width.length - 2) * (slides.length - 1)
+        } else {
+            offset -= +width.slice(0, width.length - 2)
+        }
+
+        slidesField.style.transform = `translateX(-${offset}px)`
+
+        if (slideIndex == 1) {
+            slideIndex = slides.length
+        } else {
+            slideIndex--
+        }
+
+        if (slides.length < 10) {
+            current.textContent = `0${slideIndex}`
+        } else {
+            current.textContent = slideIndex
+        }
     })
-    next.addEventListener('click', () => {
-        plusSlides(1)
-    })
+       
+    //simple slider
+
+        // showSlides(slideIndex)
+
+    //     if (slides.length < 10) {
+    //         total.textContent = `0${slides.length}`
+    //     } else {
+    //         total.textContent = slides.length
+    //     }
+      
+    // function showSlides(n) {
+    //  if (n > slides.length) {
+    //      slideIndex = 1
+    //  }
+
+    //  if (n < 1) {
+    //      slideIndex = slides.length
+    //  }
+
+    //     slides.forEach(item => {
+    //         item.classList.add('hide')
+    //     })
+
+    //     slides[slideIndex - 1].classList.remove('hide')
+
+    //     if (slides.length < 10) {
+    //         current.textContent = `0${slideIndex}`
+    //     } else {
+    //         current.textContent = slideIndex
+    //     }
+    // }
+
+    // function plusSlides(n) {
+    //     showSlides(slideIndex += n)
+    // }
+
+    // prev.addEventListener('click', () => {
+    //     plusSlides(-1)
+    // })
+    // next.addEventListener('click', () => {
+    //     plusSlides(1)
+    // })
    
 
 })
